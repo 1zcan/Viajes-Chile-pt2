c8512b38c561f5a84c13f94b87543ffb57532d72
Izcan <79218635+1zcan@users.noreply.github.com>
2024-04-08 22:24:21 -0400

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    
+  $(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+  $("#btnEnviar").click(function () {
+    alert("El correo fue enviado correctamente!");
+  });
+});
